12ac6e51d034e268505ac4f6938ccdc3366d7382
helen <hhma@alumni.cpp.edu>
2023-04-26 22:24:20 -0400

    use list instead of hard code.

--- src/main/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImpl.java
+++ src/main/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6reactiveexamples.domain.Person;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
